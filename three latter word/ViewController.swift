9b52c7fe7f9b79a78a0bfd55e99d5aeb90ec6b4e
Esmay <Molinaesme729@gmail.com>
2022-12-01 10:48:21 -0700

    Initial Commit

---
+++ three latter word/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  three latter word
+//
+//  Created by Molina, Yadira - Student on 11/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var stackview: UIStackView!
+    
+    @IBOutlet weak var currentlabel: UILabel!
+    
+    
+    @IBOutlet var wordlabel: [UILabel]!
+    
+    var letters:[String] = ["A","b","c","D","E","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        var counter = 0
+        var currentLetter: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        currentLetter = letters[counter]
+        currentlabel.text = currentLetter
+    }
+
+    
+    @IBAction func tap(_ sender: Any) {
+        let selectedPount = ((sender as! AnyObject).location(in: stackview))
+    
+        for label in wordlabel{
+    if label.frame.contains(selectedPount)
+            {
+                label.text = "\(currentLetter)"
+            }
+        }
+        
+        counter +=1
+        currentLetter 
+    }
+    
+}
+
