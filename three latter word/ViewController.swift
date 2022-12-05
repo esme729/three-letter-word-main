9e1402782e5160d7973d34fdfc0a6e85782dc2d2
Esmay <Molinaesme729@gmail.com>
2022-12-05 10:48:38 -0700

    starting writing funck

--- three latter word/ViewController.swift
+++ three latter word/ViewController.swift
@@ -18,30 +18,36 @@ class ViewController: UIViewController {
     @IBOutlet var wordlabel: [UILabel]!
     
     var letters:[String] = ["A","b","c","D","E","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-        var counter = 0
-        var currentLetter: String!
-
+    var counter = 0
+    var currentLetter: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         currentLetter = letters[counter]
         currentlabel.text = currentLetter
     }
-
+    
     
     @IBAction func tap(_ sender: Any) {
         let selectedPount = ((sender as! AnyObject).location(in: stackview))
-    
+        
         for label in wordlabel{
-    if label.frame.contains(selectedPount)
+            if label.frame.contains(selectedPount)
             {
                 label.text = "\(currentLetter)"
             }
         }
         
         counter += 1
-       // currentLetter
+        currentLetter = letters[counter]
+        currentlabel.text = currentLetter
+        
+        
     }
     
+//    func getAndSetCurrentletter(counter2: Int){currentletter = letters[couunter2]
+//
+//    }
+    
 }
-
